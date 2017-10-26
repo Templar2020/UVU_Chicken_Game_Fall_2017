b0b0fc285fb002c2661f463b11d44b1f0279ea38
Unknown <preston.powell@uvu.edu>
2017-10-25 20:22:14 -0600

    Updated Game

--- Chicken Game/Assets/Scripts/ChickenAI.cs
+++ Chicken Game/Assets/Scripts/ChickenAI.cs
@@ -2,10 +2,14 @@
 using System.Collections;
 
 public class ChickenAI : MonoBehaviour {
-	public Rigidbody enemy;
+	// public Rigidbody enemy;
 	public float moveSpeed;
 	public Transform target;
+	public Transform chickenPen;
 	
+	void Start(){
+		// chickenPen = GameObject.Find("ChickenPen");
+	}
 	void OnTriggerStay(Collider other)
 	{
 		
@@ -13,6 +17,15 @@ public class ChickenAI : MonoBehaviour {
 			Debug.Log("Player has entered chickens trigger");
 			transform.LookAt(target);
 			transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.name == "Player"){
+			/* Destroy(gameObject);*/
+			// scoreManager.AddPoints(points);
+			transform.position = chickenPen.position;
+			transform.rotation = chickenPen.rotation;
 		}
 	}
 }
