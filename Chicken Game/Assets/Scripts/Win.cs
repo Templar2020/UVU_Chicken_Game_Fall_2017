8bf561bb015913ca725b727fa75793c65dd41718
Unknown <preston.powell@uvu.edu>
2017-12-06 20:53:33 -0700

    Game Update

--- Chicken Game/Assets/Scripts/Win.cs
+++ Chicken Game/Assets/Scripts/Win.cs
@@ -5,27 +5,27 @@ using UnityEngine.UI;
 
 public class Win : MonoBehaviour {
 
-	public int winScore;
-	public GameObject scoreManager;
+	// public int winScore;
+	// public GameObject scoreManager;
 
-	int currentScore;
+	// int currentScore;
 
-	public Text winText;
+	// public Text winText;
 
-	// Use this for initialization
-	void Start () {
-		winText.GetComponent<Text>().enabled = false;
+	// // Use this for initialization
+	// void Start () {
+	// 	winText.GetComponent<Text>().enabled = false;
 		
-	}
+	// }
 	
-	// Update is called once per frame
-	void Update () {
-		currentScore = scoreManager.gameObject.GetComponent<scoreManager>().scoreAmount;
+	// // Update is called once per frame
+	// void Update () {
+	// 	currentScore = scoreManager.gameObject.GetComponent<scoreManager>().scoreAmount;
 		
-		print("Current Score = "+ currentScore);
-		if(winScore == currentScore ){
-			print("Win Score Reached = " + currentScore);
-			winText.GetComponent<Text>().enabled = true;
-		}
-	}
+	// 	print("Current Score = "+ currentScore);
+	// 	if(winScore == currentScore ){
+	// 		print("Win Score Reached = " + currentScore);
+	// 		winText.GetComponent<Text>().enabled = true;
+	// 	}
+	// }
 }
